64d3084dc185ee03764d86db6ef114fb7cdcd41d
Sukhoroslov <asukhoroslov@york.citycollege.eu>
2024-10-14 14:08:53 +0300

    Add files via upload
    Changed from a square to a rectangle.

--- Scanner.java
+++ Scanner.java
@@ -5,14 +5,16 @@ class Main
     public static void main(String[] args)
     {
         Scanner myObj = new Scanner (System.in);
-        System.out.println("Enter length of a side");
+        System.out.println("Enter length of a rectangle");
         int Length = myObj.nextInt();
+        System.out.println("Enter height of a rectangle");
+        int Height = myObj.nextInt();
         StringBuilder stars = new StringBuilder();
         for (int i = 0; i < Length; i++)
         {
-            stars. append("**");
+            stars. append("*");
         }
-        for (int x = 0; x < Length; x++)
+        for (int x = 0; x < Height; x++)
         {
             System.out.println(stars);
         }
